e1c2dafc561f198c9cc341e335bb76a71aff7970
JiHo Jeon <wjswlgh96@naver.com>
2025-02-06 16:48:47 +0900

    fix: 예외처리 빼먹은 부분 추가

--- src/main/java/com/example/jpa_todo/repository/UserRepository.java
+++ src/main/java/com/example/jpa_todo/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.example.jpa_todo.repository;
 
 import com.example.jpa_todo.entity.User;
+import com.example.jpa_todo.exception.ApplicationException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -13,11 +13,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     default User findByIdOrElseThrow(Long id) {
         return findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 아이디의 유저가 없습니다 id = " + id));
+                .orElseThrow(() -> new ApplicationException(HttpStatus.NOT_FOUND, "해당 아이디의 유저가 없습니다 id = " + id));
     }
 
     default User findByEmailAndPasswordOrThrow(String email, String password) {
         return findByEmailAndPassword(email, password)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "이메일 혹은 패스워드가 일치하지 않습니다."));
+                .orElseThrow(() -> new ApplicationException(HttpStatus.UNAUTHORIZED, "이메일 혹은 패스워드가 일치하지 않습니다."));
     }
 }
